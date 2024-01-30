8d8ad50200754468b078cd2d1c63109f3e9d6a93
liviapz <liviapavinizeviani@gmail.com>
2024-01-30 19:34:57 -0300

    Aprendendo sobre funções em Python

---
+++ Funcoes/funcoes.py
@@ -0,0 +1,13 @@
+def soma():
+
+
+    x = int(input('Por favor digite um número: '))
+    y = int(input('Por favor digite outro número: '))
+
+    calculo = (x + y)
+
+    print(f'O resultado é {calculo}')
+
+soma() #chamada da função
+
+#parâmetros
